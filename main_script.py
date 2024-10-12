5f588fd7ef82ee8c3884e5bae4f9e62b1350866c
Kill <737373Kirill@gmail.com>
2024-10-12 23:38:39 +0300

    Do homework number 5

--- main_script.py
+++ main_script.py
@@ -1,5 +1,6 @@
 import scripts.dna_rna_tools as nk_tools
 import scripts.filter_fastq as fi_fa
+import os
 from typing import List, Union, Dict, Tuple
 
 
@@ -13,22 +14,21 @@ def run_dna_rna_tools(*args: str) -> Union[str, List[str]]:
     If one sequence is passed — str,
     If multiple sequences are passed — list[str, str].
     """
-    dl = len(args)
-    Fun = args[dl - 1]
+    operation = args[- 1]
     res = []
-    for i in range(dl - 1):
-        if Fun == 'transcribe':
+    for i in range(len(args) - 1):
+        if operation == 'transcribe':
             res.append(nk_tools.transcribe(args[i]))
-        if Fun == 'reverse':
+        elif operation == 'reverse':
             res.append(nk_tools.reverse(args[i]))
-        if Fun == 'complement':
+        elif operation == 'complement':
             res.append(nk_tools.complement(args[i]))
-        if Fun == 'reverse_complement':
+        elif operation == 'reverse_complement':
             res.append(nk_tools.reverse_complement(args[i]))
-        if Fun == 'gc_cont':
+        elif operation == 'gc_cont':
             res.append(nk_tools.gc_cont(args[i]))
-        if Fun == 'DNA_or_RNA':
-            res.append(nk_tools.DNA_or_RNA(args[i]))
+        elif operation == 'dna_or_rna':
+            res.append(nk_tools.dna_or_rna(args[i]))
     if len(res) == 1:
         return res[0]
     else:
@@ -36,7 +36,8 @@ def run_dna_rna_tools(*args: str) -> Union[str, List[str]]:
 
 
 def filter_fastq(
-    seqs: Dict[str, Tuple[str, str]],
+    input_fastq: str,
+    output_fastq: str,
     gc_bounds: Union[Tuple[float, float], float] = (0, 100),
     length_bounds: Union[Tuple[int, int], int] = (0, 2**32),
     quality_threshold: float = 0
@@ -52,6 +53,7 @@ def filter_fastq(
     Returns:
         Filtered dictionary of sequences that meet all criteria.
     """
+    seqs = fi_fa.read_fastq(input_fastq)
     filtered_seqs = {}
     # Adjust boundaries if a single number is provided
     if isinstance(gc_bounds, (float, int)):
@@ -73,4 +75,6 @@ def filter_fastq(
             continue
         # If all conditions are met, add the sequence
         filtered_seqs[name] = (sequence, quality)
+    output_fastq = os.path.join("filtered", output_fastq)
+    fi_fa.write_fastq(filtered_seqs, output_fastq)
     return filtered_seqs
